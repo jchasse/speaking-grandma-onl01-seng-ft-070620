051c3dadae7068fbf93ee29b1aa0eedafaaef736
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-06 20:47:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
   if phrase.upcase == phrase
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    return 'I LOVE YOU TOO PUMPKIN!'
+    return "I LOVE YOU TOO PUMPKIN!"
   else
       return "HUH?! SPEAK UP, SONNY!"
     end
